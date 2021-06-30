87a27ee9cb16dd0cc537e97973fed5d92d7478ce
Ander <ander.ulutekin@gmail.com>
2021-06-30 21:17:32 +0300

    ToDoList Son Durum

--- app/src/main/java/com/example/todolist/ui/MainActivity.kt
+++ app/src/main/java/com/example/todolist/ui/MainActivity.kt
@@ -10,13 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
     }
-
-    /*override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }*/
 
 }
